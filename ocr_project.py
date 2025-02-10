563004513842d07d7b072f77f9a19826b6a68060
Aditya Kumar <97394509+DecodersAdii@users.noreply.github.com>
2025-02-10 21:12:37 +0530

    Update ocr_project.py

--- ocr_project.py
+++ ocr_project.py
@@ -1,9 +1,7 @@
 from PIL import Image
 import pytesseract
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+'
 
 
 # Update the Tesseract OCR path
