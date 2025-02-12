ee63417d3e5cd0324153e0ff9d849bc1b5a107c0
Aditya Kumar <97394509+DecodersAdii@users.noreply.github.com>
2025-02-12 22:29:24 +0530

    Update ocr2.py

--- ocr2.py
+++ ocr2.py
@@ -3,6 +3,8 @@ ocr_url = "https://api.aimlapi.com/vision/ocr"
 image_path = r'C:\Users\Aditya\Desktop\ocr 3\sample_image.png'
 ocr_url = "https://api.aimlapi.com/vision/ocr"
 image_path = r'C:\Users\Aditya\Desktop\ocr 3\sample_image.png'
+ocr_url = "https://api.aimlapi.com/vision/ocr"
+image_path = r'C:\Users\Aditya\Desktop\ocr 3\sample_image.png'
 
 # Your API key
 api_key = "d41f34e4922249fe8d1e0da290bcaea3"
